7b7142a9e1afc20bd5226883c02a5985f4fc572c
delanyo agbenyo <delanyodevio@gmail.com>
2020-12-11 00:12:46 +0000

    new comits

--- .eleventy.js
+++ .eleventy.js
@@ -1,42 +1,50 @@
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 // Filters
-const dateFilter = require('./src/filters/date-filter.js');
-const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const dateFilter = require("./src/filters/date-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
-const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+// Transform
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
-module.exports = config => {
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === "production";
+
+const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
+
+module.exports = (config) => {
   // Add filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
 
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/images/');
+  // Only minify HTML if in the production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
 
   // Plugins
   config.addPlugin(rssPlugin);
 
   // Returns work items, sorted by display order
-  config.addCollection('work', collection => {
-    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  config.addCollection("work", (collection) => {
+    return sortByDisplayOrder(collection.getFilteredByGlob("./src/work/*.md"));
   });
 
   // Returns work items, sorted by display order then filtered by featured
-  config.addCollection('featuredWork', collection => {
+  config.addCollection("featuredWork", (collection) => {
     return sortByDisplayOrder(
-      collection.getFilteredByGlob('./src/work/*.md')
-    ).filter(x => x.data.featured);
+      collection.getFilteredByGlob("./src/work/*.md")
+    ).filter((x) => x.data.featured);
   });
 
   // Returns a collection of blog posts in reverse date order
-  config.addCollection('blog', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  config.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
   });
 
   // Returns a list of people ordered by filename
-  config.addCollection('people', collection => {
-    return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+  config.addCollection("people", (collection) => {
+    return collection.getFilteredByGlob("./src/people/*.md").sort((a, b) => {
       return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
     });
   });
@@ -45,12 +53,12 @@ module.exports = config => {
   config.setUseGitIgnore(false);
 
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
-      input: 'src',
-      output: 'dist'
-    }
+      input: "src",
+      output: "dist",
+    },
   };
 };
